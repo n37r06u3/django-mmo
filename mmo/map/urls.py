87328253f2c70d5c07d7bc6b8adc9c8520fbd2dc
jtauber <jtauber@08cb736c-064a-0410-b5ae-0bde565e4c9b>
2008-04-04 09:33:01 +0000

    show lots in hub with link to lot if accessible

--- mmo/map/urls.py
+++ mmo/map/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
-from models import Hub
+from models import *
 
 hub_list_info = {
     'queryset': Hub.objects.all(),
@@ -9,7 +9,13 @@ hub_list_info = {
 
 hub_detail_info = hub_list_info
 
+lot_detail_info = {
+    'queryset': Lot.objects.all(),
+    'template_object_name': 'lot'
+}
+
 urlpatterns = patterns('django.views.generic.list_detail',
     (r'hub/$', 'object_list', hub_list_info),
     (r'hub/(?P<object_id>\d+)/$', 'object_detail', hub_detail_info),
+    (r'lot/(?P<object_id>\d+)/$', 'object_detail', lot_detail_info),
 )
