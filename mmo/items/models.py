f66413387874ced353c64ab7c2ef0c2ae5df9988
jtauber <jtauber@08cb736c-064a-0410-b5ae-0bde565e4c9b>
2008-04-06 23:24:25 +0000

    moved InventoryPile from items app to player app

--- mmo/items/models.py
+++ mmo/items/models.py
@@ -19,45 +19,6 @@ class ItemType(models.Model):
         pass
 
 
-class InventoryPile(models.Model): # @@@ longing for model inheritance
-    """
-    a pile of a particular type of item in a player's inventory.
-    """
-    
-    item_type = models.ForeignKey(ItemType)
-    quantity = models.PositiveIntegerField()
-    
-    player = models.ForeignKey('player.Player')
-    
-    def drop_list(self):
-        # utility method for enumerating possible quantities that can be dropped from this pile
-        return [quantity for quantity in [1, 2, 5, 10, 25, 50, 100] if quantity < self.quantity] + [self.quantity]
-    
-    def reduce(self, quantity):
-        q = min(self.quantity, quantity)
-        self.quantity -= q
-        self.save()
-        if self.quantity == 0:
-            self.delete()
-        return q
-    
-    def increase(self, quantity):
-        self.quantity += quantity
-        self.save()
-        return self.quantity
-    
-    class Meta:
-        unique_together = (
-            ('item_type', 'player'),
-        )
-    
-    def __unicode__(self):
-        return u"%s has %s [%s]" % (self.player, self.item_type, self.quantity)
-    
-    class Admin:
-        pass
-
-
 class LocationPile(models.Model): # @@@ longing for model inheritance
     """
     a pile of a particular type of item in a location.
--- mmo/player/models.py
+++ mmo/player/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from items.models import InventoryPile
-
 XP_LIST = [250, 500, 1000] # @@@
 
 class Player(models.Model):
@@ -40,5 +38,44 @@ class Player(models.Model):
     def __unicode__(self):
         return self.name
     
+    class Admin:
+        pass
+
+
+class InventoryPile(models.Model): # @@@ longing for model inheritance
+    """
+    a pile of a particular type of item in a player's inventory.
+    """
+
+    item_type = models.ForeignKey('items.ItemType')
+    quantity = models.PositiveIntegerField()
+
+    player = models.ForeignKey(Player)
+
+    def drop_list(self):
+        # utility method for enumerating possible quantities that can be dropped from this pile
+        return [quantity for quantity in [1, 2, 5, 10, 25, 50, 100] if quantity < self.quantity] + [self.quantity]
+
+    def reduce(self, quantity):
+        q = min(self.quantity, quantity)
+        self.quantity -= q
+        self.save()
+        if self.quantity == 0:
+            self.delete()
+        return q
+
+    def increase(self, quantity):
+        self.quantity += quantity
+        self.save()
+        return self.quantity
+
+    class Meta:
+        unique_together = (
+            ('item_type', 'player'),
+        )
+
+    def __unicode__(self):
+        return u"%s has %s [%s]" % (self.player, self.item_type, self.quantity)
+
     class Admin:
         pass
