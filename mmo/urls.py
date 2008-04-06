4228d1ba008e7c980d303ff61a9645c4f1999ec7
jtauber <jtauber@08cb736c-064a-0410-b5ae-0bde565e4c9b>
2008-04-06 18:00:28 +0000

    more visuals including mock-up of lot image and stats bars

--- mmo/urls.py
+++ mmo/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls.defaults import *
 
+import os.path
+
 urlpatterns = patterns('',
     (r'^map/', include('mmo.map.urls')),
     (r'^player/', include('mmo.player.urls')),
     (r'^items/', include('mmo.items.urls')),
     
     (r'^admin/', include('django.contrib.admin.urls')),
+    
+    # @@@ just for dev
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(os.path.dirname(__file__), "site_media")}),
+
 )
