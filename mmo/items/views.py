3717410d10e4ab1157a6a3d7616e53b3f6d75159
jtauber <jtauber@08cb736c-064a-0410-b5ae-0bde565e4c9b>
2008-04-17 03:55:24 +0000

    fixed issue #2 and cleaned up code a bit

--- mmo/items/views.py
+++ mmo/items/views.py
@@ -22,15 +22,21 @@ def itemtype_detail(request, itemtype_id):
     
     if request.POST:
         pass # @@@ for now
-
+    
     return render_to_response("items/itemtype_detail.html", {
         "itemtype": itemtype,
     })
+
+
 def whats_here(request, player_id):
-	# @@@ for the purposes of this activity, I'd like LocationPile to use Location, not hub/lot
-	player = get_object_or_404(Player, id=player_id)
-	if player.location.lot != None:
-		obj_list = LocationPile.objects.filter(hub=player.location.hub, lot=player.location.lot)
-	else:
-		obj_list = LocationPile.objects.filter(hub=player.location.hub, lot__isnull=True)
-	return render_to_response('items/whatshere.html', {'objects':obj_list})
+    # @@@ for the purposes of this activity, I'd like LocationPile to use Location, not hub/lot
+    player = get_object_or_404(Player, id=player_id)
+    
+    if player.location.lot != None:
+        piles = player.location.lot.what_is_here()
+    else:
+        piles = player.location.hub.what_is_here()
+    
+    return render_to_response("items/whatshere.html", {
+        "piles": piles,
+    })
