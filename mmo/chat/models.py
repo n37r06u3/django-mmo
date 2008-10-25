46c8ca38fa221899f8d396eaad8e1912a4d83f03
justinlilly <justinlilly@08cb736c-064a-0410-b5ae-0bde565e4c9b>
2008-10-25 16:41:34 +0000

    Converted the chat model over to an ABC (not 100% if this is the right choice or not). This allows us to pipe server messages such as "That's too heavy!" to the chat window.

--- mmo/chat/models.py
+++ mmo/chat/models.py
@@ -3,11 +3,37 @@ import datetime
 from mmo.player.models import Player
 
 # Create your models here.
-class Chat(models.Model):
-	""" Chat Module """
-	player = models.ForeignKey(Player)
-	message = models.CharField(blank=False, max_length=255)
-	timestamp = models.DateTimeField(blank=True, default=datetime.datetime.now())
+class GenericMessage(models.Model):
+    """ Abstract Message which represents a message and a timestamp """
+    message = models.CharField(blank=False, max_length=255)
+    timestamp = models.DateTimeField(blank=True, default=datetime.datetime.now())
+    
+    class Meta:
+        abstract = True
+        ordering = ['-timestamp']
 
-	def __unicode__(self):
-		return "(%s) %s: %s" % (self.timestamp, self.player.name, self.message)
+class ChatMessage(GenericMessage):
+    """ Global Chat Message """
+    from_player = models.ForeignKey(Player)
+    
+    def __unicode__(self):
+        return "(%s) %s: %s" % (self.timestamp, self.from_player.name, self.message)
+
+class PrivateMessage(GenericMessage):
+    """ 
+    Chat Message from one person to another 
+    Not in the original GNE
+    """
+    from_player = models.ForeignKey(Player, related_name="from")
+    to_player = models.ForeignKey(Player, related_name="to")
+    
+    def __unicode__(self):
+        return "PRIVATE: (%s) %s -> %s: %s" % (self.timestamp, self.from_player.name, self.to_player.name, self.message)
+
+class ServerMessage(GenericMessage):
+    """ Message from server to a specific player """
+    to_player = models.ForeignKey(Player)
+    
+    def __unicode__(self):
+        return "SERVER: (%s) %s: %s" % (self.timestamp, self.to_player.name, self.message)
+    
--- mmo/chat/views.py
+++ mmo/chat/views.py
@@ -1,27 +1,38 @@
 from django.shortcuts import render_to_response, get_object_or_404
 import datetime
 from mmo.player.models import Player
-from mmo.chat.models import Chat
+from mmo.chat.models import ChatMessage, ServerMessage
 
 def chat(request):
-	all_msgs = Chat.objects.order_by('-timestamp')
-	
-	return render_to_response('chat/chat_window.html', {'message':all_msgs})
+	""" displays chat output """
+    # FIXME: The way I'm combining querysets here seems hackish. Maybe I should go with MTI over ABC?
+    global_messages = ChatMessage.objects.all()[:100]
+    server_messages = ServerMessage.objects.all()[:100]
+    all_msgs = global_messages
+    all_msgs = [msg for msg in global_messages] + [msg for msg in server_messages]
+    all_msgs.sort(key=lambda x:x.timestamp)
+    
+    return render_to_response('chat/chat_window.html', {'messages':all_msgs})
 
-def update_chat(request, player_id):	
-	""" Method for inserting chat or returning recent chats """
-	if request.method == "POST":
-		player = Player.objects.get(pk=player_id)
-		# User has submitted a message, so insert it.
-		message = Chat(message=request.POST['message'], player=player, timestamp=datetime.datetime.now())
-		message.save()
-		# Only keep the 100 most recent messages in the database
-		# At least in development
-		last_100 = Chat.objects.order_by('-timestamp')[100:]
-		last_100.delete()
-	# Update the chat box now.
-	else:
-		player = Player.objects.get(pk=player_id)
-	all_msgs = Chat.objects.order_by('-timestamp')
-	
-	return render_to_response('chat/messages.html', {'messages':all_msgs, 'player':player})	
+def update_chat(request, player_id):
+    """ Method for inserting chat or returning recent chats """
+    if request.method == "POST":
+        player = Player.objects.get(pk=player_id)
+        # User has submitted a message, so insert it.
+        message = ChatMessage(message=request.POST['message'], from_player=player, timestamp=datetime.datetime.now())
+        message.save()
+        # Only keep the 100 most recent messages in the database
+        # At least in development
+        over_100 = ChatMessage.objects.order_by('-timestamp')[100:]
+        if over_100 >= 0:
+            over_100.delete()
+    # Update the chat box now.
+    else:
+        player = Player.objects.get(pk=player_id)
+        global_messages = ChatMessage.objects.all()[:100]
+        server_messages = ServerMessage.objects.all()[:100]
+        all_msgs = [msg for msg in global_messages] + [msg for msg in server_messages]
+        all_msgs.sort(key=lambda x:x.timestamp)
+        all_msgs.reverse()
+    
+    return render_to_response('chat/messages.html', {'messages':all_msgs, 'player':player})
