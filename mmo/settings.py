53c09a53a2e222fced0825c52e312b1202d95a3a
justinlilly <justinlilly@08cb736c-064a-0410-b5ae-0bde565e4c9b>
2008-11-05 17:56:46 +0000

    Adding in debug toolbar to settings.py

--- mmo/settings.py
+++ mmo/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.doc.XViewMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'mmo.urls'
@@ -83,7 +84,20 @@ INSTALLED_APPS = (
     'mmo.items',
     'mmo.chat',
     'mmo.notes',
+    'debug_toolbar',
 )
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.http_vars.HttpVarsDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.cache.CacheDebugPanel',
+)
+
 
 try:
 	from localsettings import *
